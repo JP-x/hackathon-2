f59a336a1fed27c25d2efba55243547126a0ee01
omoman <mannysanchez998@gmail.com>
2014-03-01 02:59:46 -0800

    added sample html

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express'),
 	app = express();
 
-app.get('/', function ( req, res ) {
-	res.send("<p>YES!!</p");
-});
+//use static files
+app.use( express.static( __dirname + "/static" ) );
 
+
+//start listening on server
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
 	console.log("listening on port " + port);
