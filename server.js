c4f939fea36c1236e35765a51aa3d0e9642a8b1b
omoman <mannysanchez998@gmail.com>
2014-03-01 02:47:37 -0800

    updated

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.get('/', function ( req, res ) {
 	res.send("<p>YES!!</p");
 });
 
-app.listen(8080, function () {
-	console.log("listening on port 8080");
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+	console.log("listening on port " + port);
 });
